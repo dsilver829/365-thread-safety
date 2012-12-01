2e4cb17c06bc069524fa10ee5a6ebaaf12992cda
David Silver <dsilver@candidatemetrics.com>
2012-12-01 12:25:01 -0800

    enable multithreading

---
+++ thready/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thready::Application.config.secret_token = 'd4c37145d1cf393e769d3c205b7bd53b7080f706f5dd33c5777990f95e8404ebe2dd6025004450a032624ee1c3056bd92cffa2cd2d9585503d9f3ddef41037c3'
